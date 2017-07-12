95b4242dde5a726223e362f2f4211bd805eab2dd
Oriol Mirosa <oriolmirosa@gmail.com>
2017-07-12 09:19:28 -0400

    Cleaned townhall portion of script

--- main.R
+++ main.R
@@ -8,92 +8,117 @@
 library(rvest)
 
 # Create empty vectors to fill with the links to the relevant pages
-links <- vector(mode="character", length=0)
+linksClimateChange <- vector(mode="character", length=0)
 linksBad <- vector(mode="character", length=0)
 
 # Download all the links produced with searches for 'climate change' and
 # 'global warming'. We start with 'climate change'
+# Notice that, in order to prevent Google from blocking us, we have to
+# space our connections for at least a minute. This means that downloading all
+# the pages takes a long time, about 12 hours for each search loop
 
+numLinksTotal = 0
 for (year in 2004:2017) {
   i = 0
+  numLinksYear = 0
   while (TRUE) {
     searchpage <- paste0("https://www.google.com/search?q=%22climate+change%22+site%3Atownhall.com%2Fcolumnists&rlz=1C5CHFA_enUS733US733&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F", year, "%2Ccd_max%3A12%2F31%2F", year, "&start=", i * 10)
     linkspage <- read_html(searchpage)
     linksnew <- linkspage %>% html_nodes("h3 a") %>% html_attr("href")
     if (length(linksnew) == 0) break
-    links <- c(links, linksnew)
+    linksClimateChange <- c(linksClimateChange, linksnew)
+    numLinksYear = numLinksYear + length(linksnew)
+    numLinksTotal = numLinksTotal + length(linksnew)
+    cat(paste0('Downloaded ', length(linksnew), ' links from page ', i, ' of year ', year, '\n'))
+    cat(paste0('Links downloaded for year ', year, ': ', numLinksYear, '\n'))
+    cat(paste0('Total \'global warming\' links downloaded so far: ', numLinksTotal, '\n\n'))
     i = i + 1
-    Sys.sleep(sample(seq(15, 60), 1))
+    Sys.sleep(sample(seq(60, 75), 1))
   }
 }
 
-# We have 927 links at this point:
+# We have 7304 links at this point:
 
-length(links)
+length(linksClimateChange)
+
 # Let's back these up before we move on so that we don't have to fetch them again
 
-linksTownhallClimateChange <- links
+linksTownhallClimateChange <- linksClimateChange
+save(linksTownhallClimateChange, file = 'linksTownhallClimateChange.Rda')
 
 # Now we get the links for 'global warming'
 
+linksGlobalWarming <- vector(mode="character", length=0)
+
+numLinksTotal = 0
 for (year in 2004:2017) {
   i = 0
+  numLinksYear = 0
   while (TRUE) {
     searchpage <- paste0("https://www.google.com/search?q=%22global+warming%22+site%3Atownhall.com%2Fcolumnists&rlz=1C5CHFA_enUS733US733&source=lnt&tbs=cdr%3A1%2Ccd_min%3A1%2F1%2F", year, "%2Ccd_max%3A12%2F31%2F", year, "&start=", i * 10)
     linkspage <- read_html(searchpage)
     linksnew <- linkspage %>% html_nodes("h3 a") %>% html_attr("href")
     if (length(linksnew) == 0) break
-    links <- c(links, linksnew)
+    linksGlobalWarming <- c(linksGlobalWarming, linksnew)
+    numLinksYear = numLinksYear + length(linksnew)
+    numLinksTotal = numLinksTotal + length(linksnew)
+    cat(paste0('Downloaded ', length(linksnew), ' links from page ', i, ' of year ', year, '\n'))
+    cat(paste0('Links downloaded for year ', year, ': ', numLinksYear, '\n'))
+    cat(paste0('Total \'global warming\' links downloaded so far: ', numLinksTotal, '\n\n'))
     i = i + 1
-    Sys.sleep(sample(seq(15, 60), 1))
+    Sys.sleep(sample(seq(60, 75), 1))
   }
 }
 
 # We now have a total of #, which means that we got # new links for 'global
 # warming'
 
-length(links)
-length(links) - length(linksTownhallClimateChange)
+length(linksGlobalWarming)
 
 # Once more, let's save the links as we have them now in case we need to reuse
 # them
 
-linksTownHallRaw <- links
+linksTownhallGlobalWarming <- linksGlobalWarming
+save(linksTownhallGlobalWarming, file = 'linksTownhallGlobalWarming.Rda')
+
+# Let's join together the links from the two searches
+
+linksTownhall <- c(linksTownhallClimateChange, linksTownhallGlobalWarming)
 
 # After studying the form of the links from Google, we will now clean them in
 # order to obtain the links necessary to reach the valid articles
 
 # These lines remove the Google specific elements from the URL and
 # reconstruct the URL without the encoded symbols
-links <- gsub('/url\\?q=', '', links)
-links <- gsub("(.+n[0-9]{6,7}).*", "\\1", links)
-links <- gsub("%25E2%2580%2599", "’", links)
-links <- gsub("%3Fpage%3D1", "", links)
-noNumberLinks <- which(!grepl('.+n[0-9]{6,7}$', links))
+linksTownhall <- gsub('/url\\?q=', '', linksTownhall)
+linksTownhall <- gsub("(.+n[0-9]{6,7}).*", "\\1", linksTownhall)
+linksTownhall <- gsub("%25E2%2580%2599", "’", linksTownhall)
+linksTownhall <- gsub("%3Fpage%3D1", "", linksTownhall)
+noNumberlinks <- which(!grepl('.+n[0-9]{6,7}$', linksTownhall))
 
 # These lines remove the links that are not articles (basically the author
 # pages for each Townhall author)
-for (i in length(links):1) {
-  if (!grepl("n[0-9]{6,7}", links[i]) && !grepl(".+/[0-9]{4}/[0-9]{2}/[0-9]{2}/.+", links[i])) {
-    linksBad <-append(linksBad, links[i])
-    links <- links[-i]
+for (i in length(linksTownhall):1) {
+  if (!grepl("n[0-9]{6,7}", linksTownhall[i]) && !grepl(".+/[0-9]{4}/[0-9]{2}/[0-9]{2}/.+", linksTownhall[i])) {
+    linksBad <-append(linksBad, linksTownhall[i])
+    linksTownhall <- linksTownhall[-i]
     next
   }
 }
-links <- gsub('(.+)&sa=.+', '\\1', links)
+linksTownhall <- gsub('(.+)&sa=.+', '\\1', linksTownhall)
 
 # We are left with X links
 
-length(links)
+length(linksTownhall)
 
 # Now we eliminate any duplicates, likely to appear because of the two separate
 # searches
 
-links <- unique(links)
+linksTownhall <- unique(linksTownhall)
 
 # In the end, we have X different articles
 
-length(links)
+length(linksTownhall)
 
 # Now that we have all the links for the articles, we are ready to go through
 # them and extract the relevant data
@@ -109,13 +134,13 @@ transcript <- tibble(title=character(), author=character(), date=as.Date(charact
 
 badLinks <- vector(mode='character', length=0)
 
-for (i in 1:length(links)) {
-  page <- try(read_html(links[i]))
+for (i in 1:length(linksTownhall)) {
+  page <- try(read_html(linksTownhall[i]))
   
   if (class(page) == "try-error") {
     cat(paste0("Error downloading link #: ", i, "\n"))
     transcript[i,]$title <- "error"
-    badLinks <- c(badLinks, links[i])
+    badLinks <- c(badLinks, linksTownhall[i])
     next
   }
   
@@ -129,7 +154,7 @@ for (i in 1:length(links)) {
   
   try(date <- as.Date(date))
   if (class(date) == "try-error") {
-    cat(paste0("Error converting date of link #: ", i, "\n"))
+    cat(paste0("Error converting date of link #", i, "\n"))
     date <- NA
   }
   
